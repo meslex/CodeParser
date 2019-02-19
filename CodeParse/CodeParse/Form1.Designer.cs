ad9157329b069509d23eeb12808eace7fd97ac16
meslex <ulimwegu@gmail.com>
2019-02-19 15:56:39 +0200

    first commit

---
+++ CodeParse/CodeParse/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace CodeParse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.input = new System.Windows.Forms.RichTextBox();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.buttonProcessFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.AcceptsTab = true;
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.output.Location = new System.Drawing.Point(657, 12);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(499, 531);
+            this.output.TabIndex = 0;
+            this.output.Text = "";
+            // 
+            // input
+            // 
+            this.input.AcceptsTab = true;
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.input.Location = new System.Drawing.Point(12, 12);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(499, 531);
+            this.input.TabIndex = 1;
+            this.input.Text = "";
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(518, 13);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(133, 36);
+            this.buttonOpenFile.TabIndex = 2;
+            this.buttonOpenFile.Text = "Open File";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // buttonProcessFile
+            // 
+            this.buttonProcessFile.Location = new System.Drawing.Point(518, 56);
+            this.buttonProcessFile.Name = "buttonProcessFile";
+            this.buttonProcessFile.Size = new System.Drawing.Size(133, 33);
+            this.buttonProcessFile.TabIndex = 3;
+            this.buttonProcessFile.Text = "Process File";
+            this.buttonProcessFile.UseVisualStyleBackColor = true;
+            this.buttonProcessFile.Click += new System.EventHandler(this.buttonProcessFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1168, 586);
+            this.Controls.Add(this.buttonProcessFile);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.output);
+            this.Name = "Form1";
+            this.Text = "Process File";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox output;
+        private System.Windows.Forms.RichTextBox input;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Button buttonProcessFile;
+    }
+}
+
---
+++ CodeParse/CodeParse/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace CodeParse
+{
+    public partial class Form1 : Form
+    {
+        private const string FindFunctions = @"((public|private|protected)\s|)((virtual|override)\s|)(static\s|)(\w+\[.*\]|\w+\<.*\>|\w+)\s[A-Z]\w+\(((\w+\s\w+|\w+\[.*\]\s\w+|\w+\<.*\>\s\w+)+|)\)";
+        private const string FindVariable = @"((public|private|protected)\s|)(\w+\[.*\]|\w+\<.*\>|\w+)\s[a-z]\w*(;|\s=)";
+        private const string FindClass = @"class\s[A-Z]\w+";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|cs files (*.cs)|*.cs|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    string filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        input.Text = reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        private void buttonProcessFile_Click(object sender, EventArgs e)
+        {
+            output.Text = "";
+            PrintClass();
+            PrintFunctions();
+            PrintVariables();
+        }
+
+        private void PrintFunctions()
+        {
+            output.Text += "Functions:\n";
+            foreach(Match match in Regex.Matches(input.Text, FindFunctions)){
+                output.Text += '\t'+ match.Value+';' + "\n";
+            }
+            
+        }
+
+        private void PrintVariables()
+        {
+            output.Text += "Variables:\n";
+            foreach (Match match in Regex.Matches(input.Text, FindVariable)){
+                output.Text += '\t' + Regex.Replace(match.Value, "(;|=)$", "") + "\n";
+            }
+
+        }
+
+        private void PrintClass()
+        {
+            output.Text += "Classes:\n";
+            foreach (Match match in Regex.Matches(input.Text, FindClass)){
+                output.Text += '\t' + match.Value + "\n";
+            }
+
+        }
+    }
+}
